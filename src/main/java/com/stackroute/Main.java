d7f7890fede30e7279d35b94e584f608996cfb96
shrabro <shrabro1@in.ibm.com>
2019-07-15 10:38:23 +0530

    third task

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -13,24 +13,6 @@ public class Main {
         Movie movie1 = context.getBean("movieBean1", Movie.class);
         System.out.println(movie1.getActor());
 
-        Movie movie2 = context.getBean("movieBean2", Movie.class);
-        System.out.println(movie2.getActor());
 
-        Movie movieA = context.getBean("movieA", Movie.class);
-        System.out.println(movieA.getActor());
-
-        Movie movieB = context.getBean("movieA", Movie.class);
-        System.out.println(movieB.getActor());
-
-        System.out.println(movieA == movieB);
-
-        Movie newMovieA = context.getBean("movieNewA", Movie.class);
-        System.out.println(newMovieA.getActor());
-
-        Movie newMovieB = context.getBean("movieNewA", Movie.class);
-        System.out.println(newMovieB.getActor());
-
-        System.out.println(newMovieA == newMovieB);
     }
-
 }
--- src/main/java/com/stackroute/MovieConfig.java
+++ src/main/java/com/stackroute/MovieConfig.java
@@ -2,28 +2,29 @@ package com.stackroute;
 
 import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class MovieConfig {
 
-    @Bean
+    @Autowired
+    @Qualifier("actor") //by name
     public Actor actorBean() {
         return new Actor("Matt Bomer","male",40);
     }
 
-    @Bean
+    @Autowired
+    @Qualifier("Actor") //by type
     public Actor actorBean1() {
-        return new Actor("WentWorth Miller","male",42);
+        return new Actor("Wentworth Miller","male",42);
     }
 
-    @Bean
-    public Actor actorBean2() {
-        return new Actor("Leslie Knope","female",23);
-    }
 
     @Bean
     public Movie movieBean() {
@@ -35,23 +36,6 @@ public class MovieConfig {
         return new Movie(actorBean1());
     }
 
-    @Bean
-    public Movie movieBean2() {
-        return new Movie(actorBean2());
-    }
-
-    @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
-    @Bean
-    public Movie movieA() {
-        return new Movie(actorBean());
-    }
-
-
-    @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    @Bean
-    public Movie movieNewA() {
-        return new Movie(actorBean());
-    }
 
 
 
